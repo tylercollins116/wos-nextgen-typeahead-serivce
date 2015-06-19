94723302bb7a1643764f720a4ecdcb67db06fd56
Trieu Nguyen <trieuvan@gmail.com>
2015-06-19 11:50:39 -0700

    initial model

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,23 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
+import models.Suggester;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    public static Result index(String q) {
+        return ok(index.render(q));
+    }
+    
+    
+    public static Result suggest(String query) {
+        return  ok(Json.toJson(Suggester.lookup(query, 10)));
     }
 
+    
+    
+    
 }
--- build.sbt
+++ build.sbt
@@ -3,6 +3,8 @@ name := "typeahead"
 version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
+  "org.apache.lucene" % "lucene-core" % "4.10.4",
+  "org.apache.lucene" % "lucene-suggest" % "4.10.4",
   javaJdbc,
   javaEbean,
   cache
