309d84195f18739ba6adf9e4e47e1360d10fe456
manoj.manandhar <manoj.manandhar@thomsonreuters.com>
2015-07-14 12:08:55 -0400

    Added Swagger API

--- src/test/java/com/thomsonreuters/rest/service/SuggestorTest.java
+++ src/test/java/com/thomsonreuters/rest/service/SuggestorTest.java
@@ -1,9 +1,6 @@
 package com.thomsonreuters.rest.service;
 
-
 import java.io.IOException;
-
-import javax.ws.rs.core.MediaType;
 
 import netflix.karyon.Karyon;
 import netflix.karyon.KaryonBootstrap;
@@ -13,24 +10,20 @@ import netflix.karyon.archaius.ArchaiusBootstrap;
 
 import org.codehaus.jettison.json.JSONException;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Singleton;
 import com.netflix.governator.annotations.Modules;
 import com.netflix.governator.guice.BootstrapModule;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.test.framework.AppDescriptor;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
 import com.thomsonreuters.eiddo.EiddoPropertiesLoader;
 import com.thomsonreuters.handler.HealthCheck;
 import com.thomsonreuters.injection.BootstrapInjectionModule;
+import com.thomsonreuters.injection.SwaggerHystrixModule;
 import com.thomsonreuters.injection.module.MainModule;
 import com.thomsonreuters.rest.service.SuggestorTest.TestInjectionModule.TestModule;
 
@@ -44,40 +37,45 @@ public class SuggestorTest extends JerseyTest {
 	@ArchaiusBootstrap(loader = EiddoPropertiesLoader.class)
 	@KaryonBootstrap(name = "junit", healthcheck = HealthCheck.class)
 	@Singleton
-	@Modules(include = { ShutdownModule.class, 
-			TestModule.class,
-			BootstrapInjectionModule.KaryonRxRouterModuleImpl.class, }
-	)
+	@Modules(include = { ShutdownModule.class, TestModule.class,
+			SwaggerHystrixModule.class,
+			BootstrapInjectionModule.KaryonRxRouterModuleImpl.class, })
 	public interface TestInjectionModule {
 		public static class TestModule extends MainModule {
 
 			@Override
 			protected void configure() {
-				//bind(HealthCheck.class).toInstance(mockHealthCheck);
+				// bind(HealthCheck.class).toInstance(mockHealthCheck);
 			}
 		}
 	}
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	  System.setProperty("eiddo.client.repoChain", "junit");
-    System.setProperty("eiddo.repo.junit.urlTemplate", "https://eiddo.1p.thomsonreuters.com/r/junit");
-	  //System.setProperty("eiddo.repo.junit.urlTemplate", "http://eiddo-1135711100.us-west-2.elb.amazonaws.com/r/junit");
-    System.setProperty("eiddo.repo.junit.username", "junit");
-    System.setProperty("eiddo.repo.junit.password", "junit");
-		server = Karyon.forApplication(TestInjectionModule.class,
-				(BootstrapModule[]) null);
-		server.start();
-		System.out.println("Karyon server started");
+		try {
+			System.setProperty("eiddo.client.repoChain", "junit");
+			System.setProperty("eiddo.repo.junit.urlTemplate",
+					"https://eiddo.1p.thomsonreuters.com/r/junit");
+			// System.setProperty("eiddo.repo.junit.urlTemplate",
+			// "http://eiddo-1135711100.us-west-2.elb.amazonaws.com/r/junit");
+			System.setProperty("eiddo.repo.junit.username", "junit");
+			System.setProperty("eiddo.repo.junit.password", "junit");
+			server = Karyon.forApplication(TestInjectionModule.class,
+					(BootstrapModule[]) null);
+			server.start();
+			System.out.println("Karyon server started");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		server.shutdown();
 	}
-	
+
 	public SuggestorTest() {
-	    super("com.thomsonreuters.rest.service");
+		super("com.thomsonreuters.rest.service");
 	}
 
 	@Override
@@ -85,21 +83,21 @@ public class SuggestorTest extends JerseyTest {
 		return new WebAppDescriptor.Builder().build();
 	}
 
-  @Test
-  public void testHealthCheck() throws JSONException, JsonProcessingException,
-      IOException {
-	  /*
-	   * TODO write more test
-	   */
-//    WebResource webResource = client().resource(baseUrl);
-//    ClientResponse response = webResource.path("/healthcheck")
-//        .accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-//    String json = response.getEntity(String.class);
-//    System.out.println(json);
-//    ObjectMapper m = new ObjectMapper();
-//    JsonNode root = m.readTree(json);
-//    Assert.assertNotNull(root);
-//    Assert.assertTrue(json.contains("One Platform JUnit Overriden by Eiddo"));
-  }
-	
+	@Test
+	public void testHealthCheck() throws JSONException,
+			JsonProcessingException, IOException {
+		/*
+		 * TODO write more test
+		 */
+		// WebResource webResource = client().resource(baseUrl);
+		// ClientResponse response = webResource.path("/healthcheck")
+		// .accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+		// String json = response.getEntity(String.class);
+		// System.out.println(json);
+		// ObjectMapper m = new ObjectMapper();
+		// JsonNode root = m.readTree(json);
+		// Assert.assertNotNull(root);
+		// Assert.assertTrue(json.contains("One Platform JUnit Overriden by Eiddo"));
+	}
+
 }
