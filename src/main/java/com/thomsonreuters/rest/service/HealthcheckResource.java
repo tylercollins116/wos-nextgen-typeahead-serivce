f2e2c8b6008fb555e73a715b45901040a2a02a59
manoj.manandhar <manoj.manandhar@thomsonreuters.com>
2015-07-14 11:41:24 -0400

    Added Swagger API

--- src/main/java/com/thomsonreuters/rest/service/HealthcheckResource.java
+++ src/main/java/com/thomsonreuters/rest/service/HealthcheckResource.java
@@ -1,5 +1,10 @@
 package com.thomsonreuters.rest.service;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,10 +14,6 @@ import javax.ws.rs.core.Response;
 import netflix.karyon.health.HealthCheckHandler;
 
 import com.google.inject.Inject;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiResponse;
-import com.wordnik.swagger.annotations.ApiResponses;
 
 /*
  * TODO Fix Health Check
