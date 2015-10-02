a7024428bd88d1a8fd8d87e594b2c7c6e46a9ac5
ManojManandhar <manoj.manandhar@thomsonreuters.com>
2015-10-02 14:49:33 -0400

    Update SuggesterHandler.java

--- src/main/java/com/thomsonreuters/models/SuggesterHandler.java
+++ src/main/java/com/thomsonreuters/models/SuggesterHandler.java
@@ -7,5 +7,9 @@ public interface SuggesterHandler {
 	public List<SuggestData> lookup(String query, int n);
 
 	public List<SuggestData> lookup(String path, String query, int n);
+	
+		/** added **/
+	public List<SuggestData> lookup(String query, List<String> sources,
+			List<String> infos);
 
 }
