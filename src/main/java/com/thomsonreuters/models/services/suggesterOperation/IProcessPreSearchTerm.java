1a33ebf14d4231e2d3890c8106c80ab0546f9b1f
manoj.manandhar <manoj.manandhar@thomsonreuters.com>
2016-03-02 12:24:15 -0500

    Include User PreSearched Terms in Typeahead Suggestion

--- src/main/java/com/thomsonreuters/models/services/suggesterOperation/IProcessPreSearchTerm.java
+++ src/main/java/com/thomsonreuters/models/services/suggesterOperation/IProcessPreSearchTerm.java
@@ -3,10 +3,9 @@ package com.thomsonreuters.models.services.suggesterOperation;
 public interface IProcessPreSearchTerm {
 	
 	public String[] getPreSearchedTerm(String truid, String... info);
-	
 	 
 	
-	public String[] getSearchedTerm(String[] terms,String query);
+	public String[] getSuggestions(String[] terms,String query);
 	
 
 }
