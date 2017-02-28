f0340c3f53f9088f41217fc351d9ab5364ecf2f5
manoj.manandhar <manoj.manandhar@thomsonreuters.com>
2017-02-28 16:29:26 -0500

    Health cheak upgrad for loading  dictionary

--- src/main/java/com/thomsonreuters/models/services/util/GroupTerms.java
+++ src/main/java/com/thomsonreuters/models/services/util/GroupTerms.java
@@ -1,17 +1,13 @@
 package com.thomsonreuters.models.services.util;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
-public class GroupTerms implements Property{ 
+public class GroupTerms implements Property {
 
-	public  void groupTermsBasedOnDictionary(List<String> alldictionaryInfo,Map<String, DictionaryInfo> allProperty) {
-
-	 
+	public void groupTermsBasedOnDictionary(List<String> alldictionaryInfo,
+			Map<String, DictionaryInfo> allProperty) {
 
 		for (String info : alldictionaryInfo) {
 			info = info.toLowerCase();
@@ -33,14 +29,20 @@ public class GroupTerms implements Property{
 					propertyInfo.setDictionaryName(infos[0]);
 				}
 				propertyInfo.add(infos[1], "");
+				String suggester = Property.SUGGESTER; 
+				/**
+				 * Above line doesnt have any effect but will mark here as we
+				 * will get the suggester type from eiddo
+				 **/
+				 
 				allProperty.put(infos[0], propertyInfo);
 			}
 
 		}
- 
+
 	}
 
-	public   String[] getDictionaryInfo(String data) {
+	public String[] getDictionaryInfo(String data) {
 
 		data = data.replace(Property.DICTIONARY_PATH, "");
 
@@ -49,10 +51,9 @@ public class GroupTerms implements Property{
 		return infos;
 
 	}
-	
-	
 
-	public DictionaryInfo getAllDictionaryRelatedInfos(Hashtable<String, DictionaryInfo> allProperty, String property) {
+	public DictionaryInfo getAllDictionaryRelatedInfos(
+			Hashtable<String, DictionaryInfo> allProperty, String property) {
 		if (property == null || property.trim().length() <= 0) {
 			return null;
 		}
@@ -61,21 +62,19 @@ public class GroupTerms implements Property{
 		return allProperty.get(properties[0]);
 
 	}
-	
+
 	public boolean isDictionaryRelated(String value) {
-		if (value.toLowerCase().startsWith(Property.DICTIONARY_PATH)&& (!value.toLowerCase().endsWith(Property.SUGGESTER))) {
+		if (value.toLowerCase().startsWith(Property.DICTIONARY_PATH)) {
 			return true;
 		}
 		return false;
 	}
-	
-	public String getDictionayName(String property){
-		
+
+	public String getDictionayName(String property) {
+
 		return getDictionaryInfo(property)[0];
 	}
-	
-	
-	 
+
 	@Override
 	public boolean isBucketName(String Key) {
 		return Key.toLowerCase().trim().equals(Property.S3_BUCKET);
